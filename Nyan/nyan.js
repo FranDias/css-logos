eb7702cfcffdc78b20fe82ccc3b3abe2ab840cd8
FranDias <frandias@gmail.com>
2013-01-25 13:57:31 -0500

    collect css experiments I've made from around the web

---
+++ Nyan/nyan.js
@@ -0,0 +1,7 @@
+//Meh, not listening to the grief you might give me over not css. 
+
+function songOfTheNyanPeople () {
+    var audio = document.getElementsByTagName("audio")[0];
+    audio.play();
+    audio.loop = true;
+}
